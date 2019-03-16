acc58637a62d70569e93d65aeedcadba10eb532c
Alexander <48152200+Sisha3342@users.noreply.github.com>
2019-03-16 16:14:18 +0300

    Update file_in_out.py

--- univer_prac/2_lecture/file_in_out.py
+++ univer_prac/2_lecture/file_in_out.py
@@ -1,10 +1,9 @@
-from tribon_list_with_yield import tribon_list #is it correct import?
+from tribon_list_with_yield import tribon_list
 
-from_file = open(input(), "r")
-
-num = int(from_file.read())
+with open(input(), "r") as from_file:
+    num = int(from_file.read())
+    
 list1 = tribon_list(num)
 
-to_file = open(input(), "w")
-for i in list1:
-    to_file.write(str(i) + '\n')
+with open(input(), "w") as to_file:
+    to_file.write('\n'.join(str(i) for i in list1))
