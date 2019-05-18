4e9615307c184c46e6728fdf8213a968590ab1a4
Alexander <48152200+Sisha3342@users.noreply.github.com>
2019-05-18 23:46:32 +0300

    Add files via upload

---
+++ kr/other_variants/var_12/test_.py
@@ -0,0 +1,18 @@
+def string_value(string: str):
+    value = 0
+    for c in string:
+        if c.isdigit():
+            value += int(c)
+        elif c.isalpha():
+            value += ord(c.lower()) - 96
+        else:
+            value += 1
+    return value
+
+
+open_file = open('input_txt_1.txt', "r", encoding='utf-8')
+str_list = [string.rstrip('\n') for string in open_file if string != '\n']
+str_list_with_value = list(map(lambda s: (s, string_value(s)), str_list))
+open_file.close()
+for string in sorted(str_list_with_value, key=lambda s: s[1], reverse=True):
+    print('"', string[0], '" = ', string[1], sep='')
