6654b3627b0cf74a4b5c89ef780c2b50591f780b
Sisha3342 <spalanevich@mail.ru>
2019-08-21 13:34:04 +0300

    prepared dataframe (stored in titanic.csv) for machine learning

--- machine learning/titanic/prepare_dataframe.py
+++ machine learning/titanic/prepare_dataframe.py
@@ -1,5 +1,7 @@
 import seaborn as sns
+import pandas as pd
 from collections import Counter
+from sklearn.feature_extraction import DictVectorizer
 
 if __name__ == "__main__":
     titanic = sns.load_dataset('titanic').drop(['alive', 'embark_town',
@@ -18,3 +20,13 @@ if __name__ == "__main__":
     for mask in masks:
         titanic.loc[mask, 'age'] = titanic.loc[mask, 'age'].fillna(
             titanic.loc[mask, 'age'].mean()).astype('int')
+
+    titanic.rename(columns={'sex': 'is_male'}, inplace=True)
+    sex_dict = {'male': 1, 'female': 0}
+    titanic.eval('is_male = is_male.map(@sex_dict)', inplace=True)
+
+    vec = DictVectorizer(int, sparse=False)
+    titanic = pd.DataFrame(vec.fit_transform(titanic.to_dict('records')),
+                           columns=vec.get_feature_names())
+
+    titanic.to_csv('titanic.csv')
