9e0f1f5b840649593aa2a5567bd143cad72b71dd
Sisha3342 <spalanevich@mail.ru>
2019-09-28 21:45:28 +0300

    Realized the method which calculates the determinant

--- general_python/computational_methods/lab_1/single_division.py
+++ general_python/computational_methods/lab_1/single_division.py
@@ -4,16 +4,19 @@ import numpy as np
 class SingleDivision:
 
     @staticmethod
-    def to_upper_triangle(matrix: np.ndarray):
-        pass
-
-    @staticmethod
-    def get_equation_solution(matrix: np.ndarray):
+    def get_equation_solution(matrix: np.ndarray, f_vector: np.ndarray):
         pass
 
     @staticmethod
     def get_determinant(matrix: np.ndarray):
-        pass
+        matrix_copy = matrix.copy()
+
+        for i in range(matrix_copy.shape[0]):
+            for j in range(i + 1, matrix_copy.shape[0]):
+                matrix_copy[j, i:] -= matrix_copy[j, i] / matrix_copy[i, i] \
+                                      * matrix_copy[i, i:]
+
+        return np.prod(matrix_copy.diagonal())
 
     @staticmethod
     def get_reversed_matrix(matrix: np.ndarray):
--- general_python/computational_methods/lab_1/test.py
+++ general_python/computational_methods/lab_1/test.py
@@ -1,4 +1,6 @@
 from single_division import SingleDivision
+import numpy as np
 
 if __name__ == "__main__":
-    pass
+    a = np.array([[1, 2, 3], [5, 2, 1],  [6, 6, 2]], dtype=float)
+    print(SingleDivision.get_determinant(a))
