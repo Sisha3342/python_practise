4f1b8ec04963273f9d3267368b04745dd29bb786
Sisha3342 <spalanevich@mail.ru>
2019-09-28 22:34:11 +0300

    Realized method which returns the equation solution

--- general_python/computational_methods/lab_1/single_division.py
+++ general_python/computational_methods/lab_1/single_division.py
@@ -5,7 +5,25 @@ class SingleDivision:
 
     @staticmethod
     def get_equation_solution(matrix: np.ndarray, f_vector: np.ndarray):
-        pass
+        matrix_copy = matrix.copy()
+        f_vector_copy = f_vector.copy()
+        order = matrix_copy.shape[0]
+
+        for i in range(order):
+            f_vector_copy[i] /= matrix_copy[i, i]
+            matrix_copy[i] /= matrix_copy[i, i]
+
+            for j in range(i + 1, order):
+                f_vector_copy[j] -= matrix_copy[j, i] * f_vector_copy[i]
+                matrix_copy[j, i:] -= matrix_copy[j, i] * matrix_copy[i, i:]
+
+        roots = np.array([f_vector_copy[-1]])
+
+        for i in range(order - 2, -1, -1):
+            roots = np.insert(roots, 0, f_vector_copy[i] -
+                              np.sum(roots * matrix_copy[i, i + 1:]))
+
+        return roots
 
     @staticmethod
     def get_determinant(matrix: np.ndarray):
--- general_python/computational_methods/lab_1/test.py
+++ general_python/computational_methods/lab_1/test.py
@@ -3,4 +3,5 @@ import numpy as np
 
 if __name__ == "__main__":
     a = np.array([[1, 2, 3], [5, 2, 1],  [6, 6, 2]], dtype=float)
-    print(np.matmul(SingleDivision.get_reversed_matrix(a), a))
+    f = np.array([2, 3, 4], dtype=float)
+    print(np.matmul(a, SingleDivision.get_equation_solution(a, f)) - f)
