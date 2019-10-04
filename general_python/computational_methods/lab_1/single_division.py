8fd7c4c1fcca01ca728be4c302837eea49ee256b
Sisha3342 <spalanevich@mail.ru>
2019-10-04 21:09:58 +0300

    Changed the algorithm for finding reversed matrix (added reversed way) and finding determinant

--- general_python/computational_methods/lab_1/single_division.py
+++ general_python/computational_methods/lab_1/single_division.py
@@ -9,6 +9,7 @@ class SingleDivision:
         f_vector_copy = f_vector.copy()
         order = matrix_copy.shape[0]
 
+        # straight way
         for i in range(order):
             f_vector_copy[i] /= matrix_copy[i, i]
             matrix_copy[i] /= matrix_copy[i, i]
@@ -17,6 +18,7 @@ class SingleDivision:
                 f_vector_copy[j] -= matrix_copy[j, i] * f_vector_copy[i]
                 matrix_copy[j, i:] -= matrix_copy[j, i] * matrix_copy[i, i:]
 
+        # reversed way
         roots = np.array([f_vector_copy[-1]])
 
         for i in range(order - 2, -1, -1):
@@ -28,27 +30,37 @@ class SingleDivision:
     @staticmethod
     def get_determinant(matrix: np.ndarray):
         matrix_copy = matrix.copy()
+        order = matrix.shape[0]
+        leading_elements = []
 
-        for i in range(matrix_copy.shape[0]):
-            for j in range(i + 1, matrix_copy.shape[0]):
-                matrix_copy[j, i:] -= matrix_copy[j, i] / matrix_copy[i, i] \
-                                      * matrix_copy[i, i:]
+        for i in range(order):
+            leading_elements.append(matrix_copy[i, i])
+            matrix_copy[i] /= matrix_copy[i, i]
 
-        return np.prod(matrix_copy.diagonal())
+            for j in range(i + 1, order):
+                matrix_copy[j, i:] -= matrix_copy[j, i] * matrix_copy[i, i:]
+
+        return np.prod(leading_elements)
 
     @staticmethod
     def get_reversed_matrix(matrix: np.ndarray):
         order = matrix.shape[0]
         matrix_extended = np.hstack([matrix, np.eye(order)])
 
+        # straight way
         for i in range(order):
             matrix_extended[i] /= matrix_extended[i, i]
-
-            for j in range(i):
-                matrix_extended[j, i:] -= matrix_extended[j, i] * matrix_extended[i, i:]
 
             for j in range(i + 1, order):
                 matrix_extended[j, i:] -= matrix_extended[j, i] * matrix_extended[i, i:]
 
-        return np.hsplit(matrix_extended, 2)[-1]
+        # reversed way
+        reversed_ = np.array(matrix_extended[-1, order:])
+
+        for i in range(order - 2, -1, -1):
+            reversed_ = np.vstack((matrix_extended[i, order:] -
+                                   np.sum(matrix_extended[i, i+1:order][:, np.newaxis] *
+                                          reversed_, axis=0), reversed_))
+
+        return reversed_
 
--- general_python/computational_methods/lab_1/test.py
+++ general_python/computational_methods/lab_1/test.py
@@ -2,6 +2,7 @@ from single_division import SingleDivision
 import numpy as np
 
 if __name__ == "__main__":
-    a = np.array([[1, 2, 3], [5, 2, 1],  [6, 6, 2]], dtype=float)
+    a = np.array([[1, 2, 4], [3, 1, 1],  [2, 3, 3]], dtype=float)
     f = np.array([2, 3, 4], dtype=float)
-    print(np.matmul(a, SingleDivision.get_equation_solution(a, f)) - f)
+
+    print(np.matmul(a, SingleDivision.get_reversed_matrix(a)))
