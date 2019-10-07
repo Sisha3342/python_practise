da2c77c74dcf7af881bdff10e2b824bd43fb3bec
Sisha3342 <spalanevich@mail.ru>
2019-10-07 20:14:32 +0300

    Added square root method with "empty" methods

---
+++ general_python/computational_methods/lab_2/square_root_method.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+
+class SquareRootMethod:
+
+    @staticmethod
+    def get_equation_solution(systems_matrix: np.ndarray,
+                              free_elements_vector: np.ndarray):
+        pass
+
+    @staticmethod
+    def get_determinant(matrix: np.ndarray):
+        pass
+
+    @staticmethod
+    def get_reversed_matrix(matrix: np.ndarray):
+        pass
