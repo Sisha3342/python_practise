ac092512eabde1a9703bbc9e0eef961e9c97c3d8
Sisha3342 <spalanevich@mail.ru>
2019-10-08 18:18:26 +0300

    Final version with completed roots finding method

--- general_python/computational_methods/lab_2/square_root_method.py
+++ general_python/computational_methods/lab_2/square_root_method.py
@@ -22,11 +22,35 @@ class SquareRootMethod:
         return s_matrix, d_vector
 
     @staticmethod
-    def get_equation_solution(systems_matrix: np.ndarray, free_vector: np.ndarray):
-        pass
+    def get_equation_solution(matrix: np.ndarray, free_vector: np.ndarray):
+        order = matrix.shape[0]
+
+        if (matrix.T != matrix).any():
+            free_vector = np.matmul(matrix.T, free_vector)
+            matrix = np.matmul(matrix.T, matrix)
+
+        s, d = SquareRootMethod.get_matrix_decomposition(matrix)
+
+        y = np.array([])
+        for i in range(order):
+            y = np.insert(y, i, (free_vector[i] - np.sum(y[:i] * s[:i, i])) / s[i, i])
+
+        x = np.array([])
+        for i in range(order - 1, -1, -1):
+            x = np.insert(x, 0, (y[i] - np.sum(s[i, i+1:] * d[i] * x))
+                          / (s[i, i] * d[i]))
+
+        return x
 
     @staticmethod
     def get_determinant(matrix: np.ndarray):
+        not_symmetric = (matrix.T != matrix).any()
+
+        if not_symmetric:
+            matrix = np.matmul(matrix.T, matrix)
+
         s, d = SquareRootMethod.get_matrix_decomposition(matrix)
 
-        return np.prod(s.diagonal() ** 2) * np.prod(d)
+        determinant = np.prod(s.diagonal() ** 2) * np.prod(d)
+
+        return np.sqrt(determinant) if not_symmetric else determinant
--- general_python/computational_methods/lab_2/test.py
+++ general_python/computational_methods/lab_2/test.py
@@ -2,10 +2,8 @@ from square_root_method import SquareRootMethod
 import numpy as np
 
 if __name__ == "__main__":
-    test_matrix = np.array([[1, 2, 3],
-                            [2, 5, 6],
+    test_matrix = np.array([[1, 4, 3],
+                            [4, 5, 6],
                             [3, 6, 1]])
-    a = np.array([1, 2, 3])
-    s, d = SquareRootMethod.get_matrix_decomposition(test_matrix)
-
-    print(SquareRootMethod.get_determinant(test_matrix))
+    f = np.array([1, 2, 3])
+    print(np.matmul(test_matrix, SquareRootMethod.get_equation_solution(test_matrix, f)))
