c44f569c1fd942f08f1ffd4dc98b7fc457ba0b9f
Sisha3342 <spalanevich@mail.ru>
2019-12-08 11:04:45 +0300

    initial commit

---
+++ general_python/computational_methods/lab_4/danilevsky_metod.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+
+class DanilevskyMethod:
+    def __form_matrix_m(self, matrix: np.ndarray, order: int):
+        pass
+
+    @staticmethod
+    def form_frobenius_matrix(matrix: np.ndarray):
+        pass
+
+    @staticmethod
+    def get_eigenvalues(frobenius_matrix: np.ndarray):
+        pass
+
+    @staticmethod
+    def get_eigenvectors(frobenius_matrix: np.ndarray, eigenvalues: np.ndarray):
+        pass
