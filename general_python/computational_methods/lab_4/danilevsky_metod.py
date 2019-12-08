58e78916c30005a02cb67a9b166d839e46ccbbe8
Sisha3342 <spalanevich@mail.ru>
2019-12-08 12:10:03 +0300

    Realized mathod for getting Frobenius matrix and conversion which leads to it

--- general_python/computational_methods/lab_4/danilevsky_metod.py
+++ general_python/computational_methods/lab_4/danilevsky_metod.py
@@ -1,21 +1,36 @@
 import numpy as np
+from collections import namedtuple
 
 
 class DanilevskyMethod:
     @staticmethod
-    def __form_matrix_m(matrix: np.ndarray, order: int):
+    def get_similarity_conversion(matrix: np.ndarray, order: int):
         m_matrix = np.eye(matrix.shape[0], dtype=float)
-
         m_matrix[order-2, :] = -matrix[order-1, :] / matrix[order-1, order-2]
+        m_matrix[order-2, order-2] /= -matrix[order-1, order-2]
 
-        m_matrix[order-2, order-2] *= -1
+        m_matrix_reversed = np.eye(matrix.shape[0], dtype=float)
+        m_matrix_reversed[order-2, :] = matrix[order-1]
 
-        return m_matrix
+        conversion = namedtuple("conversion", 'm_matrix reversed_m_matrix')
 
+        return conversion(m_matrix, m_matrix_reversed)
 
     @staticmethod
-    def form_frobenius_matrix(matrix: np.ndarray):
-        pass
+    def get_frobenius_and_conversion(matrix: np.ndarray):
+        matrix_a = matrix.copy()
+
+        matrix_s = np.eye(matrix.shape[0])
+
+        for i in range(matrix.shape[0], 1, -1):
+            conversion = DanilevskyMethod.get_similarity_conversion(matrix_a, i)
+            matrix_s = np.matmul(matrix_s, conversion.m_matrix)
+            matrix_a = np.matmul(np.matmul(conversion.reversed_m_matrix, matrix_a),
+                                 conversion.m_matrix)
+
+        frob_and_conv = namedtuple("frobenius_and_conversion", "frobenius_matrix conversion_matrix")
+
+        return frob_and_conv(matrix_a, matrix_s)
 
     @staticmethod
     def get_eigenvalues(frobenius_matrix: np.ndarray):
--- general_python/computational_methods/lab_4/test.py
+++ general_python/computational_methods/lab_4/test.py
@@ -2,6 +2,11 @@ import numpy as np
 from danilevsky_metod import DanilevskyMethod as DM
 
 if __name__ == "__main__":
-    matrix_a = np.array([[1, 2, 3],
-                         [3, 2, 3],
-                         [5, 5, 2]])
+    test_matrix = np.array([[8.30, 3.82, 4.10, 1.90],
+                            [3.92, 8.45, 6.58, 2.46],
+                            [3.77, 8.41, 8.04, 2.28],
+                            [2.21, 2.45, 1.69, 6.99]])
+
+    tup = DM.get_frobenius_and_conversion(test_matrix)
+    print(tup.conversion_matrix)
+
